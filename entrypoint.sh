cba997327e3175cd534248ff9d3c56dbc42442e7
Ashish <ashish@nclouds.com>
2021-07-19 06:42:22 -0700

    abc

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,13 @@ creds=$(aws sts assume-role \
   --role-session-name $role_session_name \
   --duration-seconds $duration)
 
+echo $creds
+
 aws_access_key_id=$(echo $creds | jq -r '.Credentials.AccessKeyId')
 aws_secret_access_key=$(echo $creds | jq -r '.Credentials.SecretAccessKey')
 aws_session_token=$(echo $creds | jq -r '.Credentials.SessionToken')
+
+echo "Before Mask"
 
 echo ::add-mask::$aws_access_key_id
 echo ::add-mask::$aws_secret_access_key
@@ -29,3 +33,5 @@ echo "Executing  set-output"
 echo ::set-output name=AWS_ACCESS_KEY_ID::$aws_access_key_id
 echo ::set-output name=AWS_SECRET_ACCESS_KEY::$aws_secret_access_key
 echo ::set-output name=AWS_SESSION_TOKEN::$aws_session_token
+
+echo "After set-output"
